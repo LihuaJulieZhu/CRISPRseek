1032ef8bc5f17d8fe38fb13745e3d01e70868a87
Julie Zhu <julie.zhu@umassmed.edu>
2018-06-29 13:35:35 -0400

    added exception handing for uniqueREs when no perfect target site found

--- R/uniqueREs.R
+++ R/uniqueREs.R
@@ -21,21 +21,28 @@ uniqueREs <- function(REcutDetails, summary, offTargets,
 	   for (i in 1:length(Start))		
 	   {
 	       thisChr <-chr[i]
-   	       thisEnd <- min(End[i], seqlengths(BSgenomeName)[thisChr][[1]])
-	       thisStart <- max(1, Start[i])
- 	       thisStrand <- as.character(strand[i])
-	       scanSequence <- BSgenome::getSeq(BSgenomeName, 
-                   thisChr, start = thisStart, 
-	           end = thisEnd, strand = thisStrand, 
-	           as.character = TRUE)
-               REnames <- unlist(strsplit(as.character(summary$REname[i]), " "))
-	       REpatterns <- unique(
-	           REcutDetails$REpattern[as.character(
+               if (!is.na(thisChr) && thisChr != "")
+               {
+   	          thisEnd <- min(End[i], seqlengths(BSgenomeName)[thisChr][[1]])
+	          thisStart <- max(1, Start[i])
+ 	          thisStrand <- as.character(strand[i])
+	          scanSequence <- BSgenome::getSeq(BSgenomeName, 
+                     thisChr, start = thisStart, 
+	             end = thisEnd, strand = thisStrand, 
+	             as.character = TRUE)
+                  REnames <- unlist(strsplit(as.character(summary$REname[i]), " "))
+	          REpatterns <- unique(
+	              REcutDetails$REpattern[as.character(
                        REcutDetails$REname) %in% REnames])
-	       REnames <- REwithName[REwithName[,1] %in% REpatterns,2]
-	       REs[i] <- paste(unique(
-		   REnames[isPatternUnique(scanSequence, 
-                   DNAStringSet(REpatterns)) == "Yes"]), collapse= " ")
+	          REnames <- REwithName[REwithName[,1] %in% REpatterns,2]
+	          REs[i] <- paste(unique(
+		     REnames[isPatternUnique(scanSequence, 
+                     DNAStringSet(REpatterns)) == "Yes"]), collapse= " ")
+               }
+               else
+               {
+                 REs[i] = ""
+               }
 	   }
 	}
 	REs
